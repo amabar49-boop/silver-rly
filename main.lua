54b41a2ae64ec29e8564411a31b6030079cc5be4
amabar49-boop <amabar49@gmail.com>
2026-01-29 12:08:10 +0700

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,31 @@
+-- Load Library
+local Library = loadstring(game:HttpGet("https://script.vinzhub.com/library"))()
+
+-- Create Window
+local Window = Library:CreateWindow({
+    Title = "SilverHub",
+    Center = true,
+    AutoShow = true
+})
+
+-- Create Tab
+local MainTab = Window:AddTab("Main")
+
+-- Button
+MainTab:AddButton("Test Button", function()
+    print("Button ditekan")
+end)
+
+-- Toggle
+MainTab:AddToggle("Test Toggle", false, function(Value)
+    print("Toggle:", Value)
+end)
+
+-- Slider
+MainTab:AddSlider("WalkSpeed", {
+    Min = 16,
+    Max = 100,
+    Default = 16
+}, function(Value)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
+end)
